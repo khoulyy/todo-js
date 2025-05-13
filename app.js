5d5326882390f463c4128f740d678257c447ff29
Abdelrahman Elkhouly <khoulyyy@gmail.com>
2025-05-13 21:49:03 +0300

    Replace let with const

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 // Get elements
-let inputField = document.querySelector(".input-text");
-let tbody = document.querySelector("tbody");
-let addButton = document.querySelector(".add");
+const inputField = document.querySelector(".input-text");
+const tbody = document.querySelector("tbody");
+const addButton = document.querySelector(".add");
 
 addButton.addEventListener("click", (e) => {
     if (inputField.value == "") {
         alert("Please Enter a Task");
     } else {
-        let checkbox = document.createElement("input");
+        const checkbox = document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
 
-        let checkCell = document.createElement("td");
+        const checkCell = document.createElement("td");
         checkCell.appendChild(checkbox);
 
-        let taskCell = document.createElement("td");
+        const taskCell = document.createElement("td");
         taskCell.setAttribute("class", "TaskName");
 
-        let deleteButton = document.createElement("button");
+        const deleteButton = document.createElement("button");
         deleteButton.setAttribute("class", "delete");
         deleteButton.textContent = "Del";
 
-        let deleteCell = document.createElement("td");
+        const deleteCell = document.createElement("td");
         deleteCell.appendChild(deleteButton);
 
-        let tr = document.createElement("tr");
+        const tr = document.createElement("tr");
         taskCell.textContent = inputField.value;
         tr.append(checkCell, taskCell, deleteCell);
         tbody.appendChild(tr);
